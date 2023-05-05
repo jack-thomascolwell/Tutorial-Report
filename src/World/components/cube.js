9bf3bdea34ad8eee69feedb6a5b4b594afe4cc2e
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2023-05-05 16:16:03 -0700

    1.8. Texture Mapping

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -1,9 +1,9 @@
-import { BoxBufferGeometry, Mesh, MeshStandardMaterial, MathUtils } from 'three';
+import { BoxBufferGeometry, Mesh, MeshStandardMaterial, MathUtils, TextureLoader } from 'three';
 const radiansPerSecond = MathUtils.degToRad(30);
 
 function createCube() {
     const geometry = new BoxBufferGeometry(2, 2, 2);
-    const material = new MeshStandardMaterial({ color: 'purple' });
+    const material = createMaterial();
     const cube = new Mesh(geometry, material);
     cube.rotation.set(-0.5, -0.1, 0.8);
     cube.tick = (delta) => {
@@ -12,6 +12,14 @@ function createCube() {
         cube.rotation.y += radiansPerSecond * delta;
     };
     return cube;
+}
+
+function createMaterial() {
+    const textureLoader = new TextureLoader();
+    const texture = textureLoader.load('/assets/textures/uv-test-bw.png');
+
+    const material = new MeshStandardMaterial({ map: texture });
+    return material;
 }
 
 export { createCube };
