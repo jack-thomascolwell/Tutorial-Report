015187e632f1c9be653d3cabd803b8de8a1d6f82
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2023-05-05 15:35:33 -0700

    1.4. Lighting

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -1,9 +1,10 @@
-import { BoxBufferGeometry, Mesh, MeshBasicMaterial } from 'three';
+import { BoxBufferGeometry, Mesh, MeshStandardMaterial } from 'three';
 
 function createCube() {
     const geometry = new BoxBufferGeometry(2, 2, 2);
-    const material = new MeshBasicMaterial();
+    const material = new MeshStandardMaterial({ color: 'purple' });
     const cube = new Mesh(geometry, material);
+    cube.rotation.set(-0.5, -0.1, 0.8);
     return cube;
 }
 
