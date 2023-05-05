138750214ed29990f167312bc83587a43725d1eb
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2023-05-05 16:41:26 -0700

    1.10. Ambient Lighting
    Modified lights.js to use a group of lights rather than return multiple individual lights

--- src/World/components/lights.js
+++ src/World/components/lights.js
@@ -1,9 +1,21 @@
-import { DirectionalLight } from 'three';
+import { AmbientLight, DirectionalLight, HemisphereLight, Group } from 'three';
 
 function createLights() {
     const light = new DirectionalLight('white', 8);
-    light.position.set(10, 10, 10);
-    return light;
+    const ambientLight = new HemisphereLight(
+        'white', // bright sky color
+        'darkslategrey', // dim ground color
+        5, // intensity
+    );
+
+    const mainLight = new DirectionalLight('white', 5);
+    mainLight.position.set(10, 10, 10);
+
+    const lights = new Group();
+    lights.add(ambientLight);
+    lights.add(mainLight);
+
+    return lights;
 }
 
 export { createLights };
