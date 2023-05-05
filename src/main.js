d4844e20ef58ec38c246ff4beea5338c579d4f9b
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2023-05-05 15:25:40 -0700

    1.3. World App

--- src/main.js
+++ src/main.js
@@ -1,37 +1,9 @@
-import {
-    BoxBufferGeometry,
-    Color,
-    Mesh,
-    MeshBasicMaterial,
-    PerspectiveCamera,
-    Scene,
-    WebGLRenderer,
-  } from 'three';
+import { World } from './World/World.js';
 
-const container = document.getElementById('scene-container');
-const scene = new Scene();
+function main() {
+    const container = document.getElementById('scene-container');
+    const world = new World(container);
+    world.render();
+}
 
-scene.background = new Color('skyblue');
-
-// camera
-const fov = 35;
-const aspect = container.clientWidth / container.clientHeight;
-const near = 0.1; //near clipping
-const far = 100; //far clipping
-const camera = new PerspectiveCamera(fov, aspect, near, far);
-camera.position.set(0, 0, 10);
-
-// create cube
-const geometry = new BoxBufferGeometry(2, 2, 2);
-const material = new MeshBasicMaterial();
-const cube = new Mesh(geometry, material);
-scene.add(cube);
-
-// renderer
-const renderer = new WebGLRenderer();
-renderer.setSize(container.clientWidth, container.clientHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-container.append(renderer.domElement);
-
-// render
-renderer.render(scene, camera);
+main();
